4416eb4aa1275dfea4154aa39d309a313392513c
github_cloudlabsuser_2053 <github_cloudlabsuser_2053@cloudlabsaiuser.com>
2025-05-29 18:12:23 +0000

    Refactor code structure for improved readability and maintainability

--- MyMvcApp.Tests/UserControllerTests.cs
+++ MyMvcApp.Tests/UserControllerTests.cs
@@ -1,7 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
 using MyMvcApp.Controllers;
 using MyMvcApp.Models;
-using Microsoft.AspNetCore.Http;
 
 namespace MyMvcApp.Tests;
 
